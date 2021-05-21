d107650c831adc9c2e8d97b2d34304f6285ce8d0
Minerva-Gutierrez <201721115@tese.edu.mx>
2021-05-21 10:49:54 -0500

    EQUIPO 1 HERO SERVICE

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -3,9 +3,9 @@ import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
   { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Super Man' },
+  { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Btman' },
+  { id: 14, name: 'Celeritas' },
   { id: 15, name: 'Magneta' },
   { id: 16, name: 'RubberMan' },
   { id: 17, name: 'Dynama' },
